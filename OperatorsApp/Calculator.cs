b2a09141268772bc9491d6a32f72957864b5b23e
Kaushal55555 <lordqshal@gmail.com>
2025-11-11 11:18:17 +0545

    Add project files.

---
+++ OperatorsApp/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsApp
+{
+    internal class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be zero.");
+            }
+            return (double)a / b;
+        }
+        public void OddEvenFinder(int number)
+        {
+            string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ OperatorsApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OperatorsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of Calculator class
+            Calculator calc = new Calculator();
+
+            // Perform operations
+            Console.WriteLine("Addition: " + calc.Add(10, 5));
+            Console.WriteLine("Subtraction: " + calc.Subtract(10, 5));
+            Console.WriteLine("Multiplication: " + calc.Multiply(10, 5));
+            Console.WriteLine("Division: " + calc.Divide(10, 5));
+
+            // Check Odd or Even
+            Console.Write("Enter a number to check Odd or Even: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            calc.OddEvenFinder(number);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
